2e22bbea21b9f9116ee4705a0cd01e3445f74ac1
Ysk-dev <yusufsertkayaysk@gmail.com>
2024-12-06 21:22:08 +0300

    applications

--- applicatinOfGenerators.py
+++ applicatinOfGenerators.py
@@ -1,27 +1,66 @@
-def createNumber():
-    num = 0
-    while True:
-        yield num ** 2
-        num +=1
+# def createNumber():
+#     num = 0
+#     while True:
+#         yield num ** 2
+#         num +=1
 
-generator = createNumber()
+# generator = createNumber()
 
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
+# for i in generator:
+#     print(i)
 
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
 
-print(next(generator))
+def fiboList(max):
+    numbers = []
+
+    a, b = 0,1
+
+
+    while len(numbers) <= max:
+        numbers.append(b)
+        a,b =b,a+b
+
+    return numbers
+
+#print(fiboList(15))
+
+def fiboGen(max):
+    a, b=0,1
+    counter = 0
+
+    while counter <= max:
+        a,b=b,a+b
+
+        yield b
+
+        counter += 1
+ 
+# fiboGenerator = fiboGen(66)
+# for i in fiboGenerator:
+#     print(i)
+
+import sys
+listGen = [i for i in range(10000)]
+
+print(sys.getsizeof(listGen))
+print("------------------------")
+gen = (i for i in range(10000))
+
+print(sys.getsizeof(gen))
+print("------------------------")
+
+import time 
+
+listStartTime = time.time()
+sum([i for i in range(900000)])
+listStop = time.time() - listStartTime
+
+print("-----------------------")
+
+genStartTime = time.time()
+sum((i for i in range(900000)))
+genStop = time.time() - genStartTime
+
+print(listStop)
+print("---------------------")
+print(genStop)
