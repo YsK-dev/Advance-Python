8fb66f26e76823097b56e2086ac3ff119971d67c
Ysk-dev <yusufsertkayaysk@gmail.com>
2024-12-09 21:48:43 +0300

    decorators in functions

---
+++ functions/decoratorFunction.py
@@ -0,0 +1,17 @@
+def decGreet(count):
+    def greet(fn):
+        def inner(name):
+            for _ in range(count):
+                fn(name)
+        return inner
+    return greet
+@decGreet(count = 2)
+def gm(name):
+    print(f"Good morning my name is {name}")
+
+@decGreet(count=4)
+def gd(name):
+    print(f"Good days my name is {name}")
+
+gm("yusuf")
+gd("yakup")
