74c636f3956d6342be6f24ee97e6c61a3851c2d4
Ysk-dev <yusufsertkayaysk@gmail.com>
2025-02-11 12:10:01 +0300

    Feat: aggregate functions Count()

---
+++ Database/aggregateFunctions.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Asd171307.",
+    database="yskweb"
+)
+
+cursor = db.cursor()
+
+sql ="Select Count(*) from products"
+
+cursor.execute(sql)
+
+
+result = cursor.fetchone()
+
+print(result)
